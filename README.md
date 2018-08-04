772c0db996473d2a6878210ea45f901d8608e9df
Luke Smith <luke@lukesmith.xyz>
2018-08-04 14:34:31 -0400

    first stuff

---
+++ README.md
@@ -0,0 +1,35 @@
+# The LARBS Post-Install Wizard
+
+**NOTE TO PEOPLE WHO AREN'T LUKE:** This isn't ready for use yet. It's just up
+on Github now for my convenience as I'm testing things. I'll say when it's
+ready. You're welcome to check out the idea and provide feedback, but don't
+expect it to werk.
+
+This is a brief dialog menu that allows the user to install additional programs
+and change minor settings.
+
+## The Backend
+
+The `menu.sh` is the main interface. It reads the `choices.csv` file for the
+package sets or other possible choices in the menu and constructs a `dialog`
+menu out of them.
+
+The user can then choose an option. The script will install the programs
+corresponding to that choice's tag that are listed in `installable.csv`.
+
+You can also set up wrapping scripts in `wrappers/`. For example, let's say the
+user choices the "mutt wizard" option which is tagged with `E`. Once chosen,
+the script will check to see if the files `wrappers/E.pre` and
+`wrappers/E.post` exist. If `E.pre` exists, it will be run after the choice,
+but before the installation. If `E.post` exists, it will be run after the
+installation of the programs.
+
+In the case of mutt-wizard for example, `E.pre` checks to see if the user
+already has a `~/.config/mutt/` directory and files and will as to back them
+up, then download the `mutt-wizard` repository which goes in its place.
+`E.post` will give additional directions for using mutt-wizard.
+
+## Possible extentions
+
+Right now, the script only installs additional things. It might be worth it to
+be able to log the options already run and make them undoable, etc.
---
+++ wizard.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Add explanation.
+
+menufile="$HOME/.larbs-wizard/choices.csv"
+progsfile="$HOME/.larbs-wizard/installable.csv"
+specdir="$HOME/.larbs-wizard/wrappers"
+
+# Give packerwrapper a list of programs. Finds only those needing installation
+# then, it sorts them into $mainqueue and $aurqueue for progs installable from
+# main repo or AUR.
+posprogs=$(pacman -Sl | awk '{print $2}')
+packerwrapper() { \
+	for arg in "$@"
+	do
+		pacman -Q "$arg" &>/dev/null && continue
+		pacman -Qg "$arg" &>/dev/null && continue
+		grep "^$arg$" <<< $posprogs &>/dev/null  && mainqueue="$mainqueue $arg" && continue
+		aurqueue="$queue $arg"
+	done ;}
+
+tmpdir=$(mktemp -d)
+
+# Construct menu file.  For some reasons, it's easier to constuct it on the fly
+# than use eval and such.
+echo "dialog --title \"LARBS Post-Install Wizard\" --menu \"What would you like to do?\" 15 45 8 \\" > $tmpdir/menu.sh
+echo $(cut -d, -f1,2 "$menufile" | sed -e "s/,/ \"/g;s/$/\"/g")" \\" >> $tmpdir/menu.sh
+echo "2>$tmpdir/choice" >> $tmpdir/menu.sh
+
+# Get user input of what packages to install.
+bash $tmpdir/menu.sh
+chosen=$(cat $tmpdir/choice)
+[[ $chosen == "" ]] && clear && exit
+
+# In addition to installing the tagged programs, you can have scripts that run
+# either before or after the installation.  To do this, you need only create a
+# file in ~/.larbs-wizard/.specific/Z.pre (or Z.post).  `Z` here is the tag of
+# the programs.
+
+[[ -f  $specdir/$chosen.pre ]] && bash $specdir/$chosen.pre
+
+# Quit script if preinstall script returned error or if user ended it.
+[[ ! $? -eq 0 ]] && clear &&  exit
+
+# Run the `packerwrapper` script on all the programs tagged with the chosen tag
+# in the progs file.
+packerwrapper $(grep "^$chosen" "$progsfile" | cut -d ',' -f2)
+
+clear
+# If there
+[[ ! "$mainqueue" == "" ]] && sudo pacman --needed --noconfirm -S $mainqueue
+[[ ! "$aurqueue" == "" ]] && packer --noconfirm -S $aurqueue
+
+# Post installation script.
+[[ -f  $specdir/$chosen.post ]] && bash $specdir/$chosen.post
